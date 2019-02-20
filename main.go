412ab0d456ba97b4e8694e0f05133bfea5c7f931
Josiah Campbell <jocmp64@gmail.com>
2019-02-19 22:36:54 -0600

    Print next Run number at Western Brown Line

--- main.go
+++ main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+// Arrival describes an individual arrival time prediction
+type Arrival struct {
+	Run string `json:"rn"`
+}
+
+// TrainTracker contains a grouping of etas
+type TrainTracker struct {
+	Arrivals []Arrival `json:"eta"`
+}
+
+// Response of the TT API
+type Response struct {
+	TrainTracker TrainTracker `json:"ctatt"`
+}
+
 func main() {
-	response, err := http.Get(fmt.Sprintf("http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=%s&mapid=40380", KEY))
+	westernMapID := 41480
+	response, err := http.Get(fmt.Sprintf("http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=%s&mapid=%d&outputType=JSON", KEY, westernMapID))
 	if err != nil {
-		fmt.Println(err);
+		fmt.Println(err)
 		return
 	}
+
 	defer response.Body.Close()
-	if response.StatusCode == http.StatusOK {
-		responseBody, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(responseBody))
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Printf("error fetching:%d", response.StatusCode)
 	}
+
+	rb, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r := &Response{}
+	err = json.Unmarshal(rb, r)
+
+	fmt.Println(r.TrainTracker.Arrivals[0].Run)
 }
